034ef129b8ce25b8c7de40cc0d424a9559f83a66
nadzal <nadzalhamdzah5141@gmail.com>
2025-10-09 11:43:26 +0700

    feat(models/applicantModels): penambahan model applicant

---
+++ server/models/applicantModel.js
@@ -0,0 +1,84 @@
+import { Model, DataTypes } from 'sequelize';
+import sequelize from "../config/database.js"
+
+class Applicant extends Model {
+  static associate(models) {
+    // Definisikan hubungan di sini
+    this.hasMany(models.ApplicantScore, { 
+      foreignKey: 'applicantId',
+      onDelete: 'CASCADE' 
+    });
+  }
+}
+
+Applicant.init({
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  photo: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  fullName: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  nickName: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true
+    }
+  },
+  gender: {
+    type: DataTypes.ENUM('Laki-laki', 'Perempuan'),
+    allowNull: false
+  },
+  birthDate: {
+    type: DataTypes.DATEONLY,
+    allowNull: false,
+    defaultValue: '1970-01-01'
+  },
+  socialMedia: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  linkedin: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  phone: {
+    type: DataTypes.STRING(20),
+    allowNull: false
+  },
+  province: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  city: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  addressByKtp: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  currentAddress: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  }
+}, {
+  sequelize,
+  modelName: 'Applicant',
+  tableName: 'applicants',
+  timestamps: true
+});
+
+export default Applicant;
