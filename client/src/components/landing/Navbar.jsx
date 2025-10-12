2b14dab25edf83c9b72bb40138d44dfcf851ab90
nadzal <nadzalhamdzah5141@gmail.com>
2025-10-12 21:29:27 +0700

    fix(navbar): perbaikan agar ketika link diklik akan terbuka tab baru

--- client/src/components/landing/Navbar.jsx
+++ client/src/components/landing/Navbar.jsx
@@ -118,6 +118,8 @@ export default function Navbar() {
             <Link
               to="/skill-connect"
               className="hover:text-blue-600 transition-colors duration-200 relative group"
+              target="_blank"
+              rel="noopener noreferrer"
             >
               Punya Skill Connect
               <span className="absolute -bottom-1 left-0 w-0 h-0.5 bg-blue-600 transition-all duration-300 group-hover:w-full"></span>
