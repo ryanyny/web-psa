6db2f24f115a034d2b0d45b3af9869fdf21f75d1
nadzal <nadzalhamdzah5141@gmail.com>
2025-10-17 01:48:09 +0700

    fix(pages/ApplicantFormPage): perbaikan pada halaman ke 9 dna penambahan component baru

--- client/src/pages/screening/ApplicantFormPage.jsx
+++ client/src/pages/screening/ApplicantFormPage.jsx
@@ -9,6 +9,7 @@ import Step5Work from "../../components/screening/Step5Work.jsx";
 import Step6JobPreference from "../../components/screening/Step6JobPreference.jsx";
 import Step7JobReadiness from "../../components/screening/Step7JobReadiness.jsx";
 import Step8Aggrement from "../../components/screening/Step8Aggrement.jsx";
+import ThankYou from "../../components/screening/ThankYou.jsx";
 
 const ApplicantFormPage = () => {
   const [step, setStep] = useState(1);
@@ -271,18 +272,7 @@ const handleSubmit = async () => {
         );
       case 9:
         return (
-          <div className="text-center p-10">
-            <h2 className="text-2xl font-bold text-green-600 mb-4">
-              Terima Kasih!
-            </h2>
-            <p className="text-gray-700">{message.text}</p>
-            <Link
-              to="/skill-connect"
-              className="mt-6 inline-block px-6 py-2 bg-blue-600 text-white rounded"
-            >
-              Kembali ke Form
-            </Link>
-          </div>
+          <ThankYou message={message} />
         );
       default:
         return <div>Formulir Tidak Ditemukan.</div>;
